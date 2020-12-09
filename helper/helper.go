fa0ae98fd6bdfa4ac5d894e242359160748b2305
Firmansyah Adiputra <frm.adiputra@gmail.com>
2020-12-09 16:11:46 +0700

    Add more helpers

--- helper/helper.go
+++ helper/helper.go
@@ -4,10 +4,12 @@ import (
 	"crypto/md5"
 	"errors"
 	"fmt"
+	"regexp"
 	"strings"
 )
 
-func HashedNim(s string) (string, error) {
+// HashedEmailToNim mengkonversikan data email menjadi NIM kemudian di-hash.
+func HashedEmailToNim(s string) (string, error) {
 	nim, err := EmailToNim(s)
 	if err != nil {
 		return "", err
@@ -16,7 +18,7 @@ func HashedNim(s string) (string, error) {
 	return SaltedHash(nim)
 }
 
-// EmailToNim convert email to NIM
+// EmailToNim mengkonversikan data email menjadi NIM
 func EmailToNim(s string) (string, error) {
 	idx := strings.Index(s, "@")
 	if idx == -1 {
@@ -26,9 +28,77 @@ func EmailToNim(s string) (string, error) {
 	return s[0:idx], nil
 }
 
-// SaltedHash akan memberikan hasil berupa nilai v yang diberi salt kemudian di-hash.
+// JenisEmail memberikan return value "student" jika dan hanya jika email
+// menggunakan domain "student.trunojoyo.ac.id". Selain itu akan memberikan
+// return value "bukan student".
+func JenisEmail(s string) (string, error) {
+	idx := strings.Index(s, "@")
+	if idx == -1 {
+		return "", errors.New("character '@' not found")
+	}
+
+	if s[idx+1:] == "student.trunojoyo.ac.id" {
+		return "student", nil
+	}
+
+	return "bukan student", nil
+}
+
+// SaltedHash akan memberikan hasil berupa nilai v yang ditambahi salt kemudian di-hash.
 func SaltedHash(v string) (string, error) {
 	bSalted := []byte(salted(v))
 	bSum := md5.Sum(bSalted)
 	return fmt.Sprintf("%x", bSum), nil
+}
+
+// Last3UpperSaltedHash mengambil tiga karakter terakhir, dikonversikan menjadi
+// huruf besar, kemudian ditambahi dengan salt dan di-hash.
+func Last3UpperSaltedHash(v string) (string, error) {
+	s0, err := last3Upper(v)
+	if err != nil {
+		return "", err
+	}
+
+	return SaltedHash(s0)
+}
+
+// AlphabetOnlyLast3UpperSaltedHash mengambil hanya karakter huruf, kemudian
+// diambil tiga karakter terakhir, dikonversikan menjadi huruf besar, kemudian
+// ditambahi dengan salt dan di-hash.
+func AlphabetOnlyLast3UpperSaltedHash(v string) (string, error) {
+	s0, err := alphabetOnly(v)
+	if err != nil {
+		return "", err
+	}
+
+	s1, err := last3Upper(s0)
+	if err != nil {
+		return "", err
+	}
+
+	return SaltedHash(s1)
+}
+
+// alphabetOnly akan memberikan return value berupa string yang hanya berisi
+// huruf saja dari string `v` yang menjadi inputannya.
+func alphabetOnly(v string) (string, error) {
+	// Gunakan Regex mengambil hanya huruf
+	reg, err := regexp.Compile("[^a-zA-Z]+")
+	if err != nil {
+		return "", err
+	}
+	s := reg.ReplaceAllString(v, "")
+	return s, nil
+}
+
+// last3Upper mengambil tiga karakter terakhir pada string dan mengubahnya
+// menjadi huruf besar. Bila string `v` yang menjadi input memiliki panjang
+// kurang dari 3 karakter, maka akan mengembalikan error.
+func last3Upper(v string) (string, error) {
+	if len(v) < 3 {
+		return "", errors.New("String harus memiliki panjang minimal 3 karakter")
+	}
+
+	s0 := string(v[len(v)-3:])
+	return strings.ToUpper(s0), nil
 }
--- helper/helper_test.go
+++ helper/helper_test.go
@@ -1,20 +1,150 @@
 package helper
 
 import (
-	"crypto/md5"
-	"fmt"
 	"testing"
 )
 
-func TestSaltedHash(t *testing.T) {
-	expected := fmt.Sprintf("%x", (md5.Sum([]byte("123456789contohsalt"))))
-	found, err := SaltedHash("123456789")
-
-	if err != nil {
-		t.Fatal(err)
+func TestAlphabetOnly(t *testing.T) {
+	params := []string{
+		"Pemilu 2020 UTM!",
+		"  Pemilu 2020 UTM! ",
+		"@#  P$7e&m,il.u 20,20 U.T.M! ",
 	}
+	expected := "PemiluUTM"
+	for _, v := range params {
+		found, err := alphabetOnly(v)
+		if err != nil {
+			t.Errorf("Should not be error on params '%s': %s", v, err)
+		} else {
+			if found != expected {
+				t.Errorf("On input '%s', found '%s', expected '%s'", v, found, expected)
+			}
+		}
+	}
+}
 
-	if expected != found {
-		t.Error("Hash result not equal")
+func TestLast3Upper(t *testing.T) {
+	params := []string{
+		"abcdef",
+		"ABCDEF",
+		"AbCdEf",
+		"aBcDeF",
+	}
+	expected := "DEF"
+	for _, v := range params {
+		found, err := last3Upper(v)
+		if err != nil {
+			t.Errorf("Should not be error on params '%s': %s", v, err)
+		} else {
+			if found != expected {
+				t.Errorf("On input '%s', found '%s', expected '%s'", v, found, expected)
+			}
+		}
+	}
+}
+
+func TestLast3UpperError(t *testing.T) {
+	params := []string{
+		"",
+		"A",
+		"Ab",
+	}
+	for _, v := range params {
+		_, err := last3Upper(v)
+		if err == nil {
+			t.Errorf("Should be returning error on params '%s'", v)
+		}
+	}
+}
+
+func TestEmailToNim(t *testing.T) {
+	params := []string{
+		"123456789012@trunojoyo.ac.id",
+		"123456789012@student.trunojoyo.ac.id",
+		"saya@trunojoyo.ac.id",
+		"saya@student.trunojoyo.ac.id",
+		"mereka@gmail.com",
+	}
+	expecteds := []string{
+		"123456789012",
+		"123456789012",
+		"saya",
+		"saya",
+		"mereka",
+	}
+	for i := 0; i < len(params); i++ {
+		v := params[i]
+		expected := expecteds[i]
+
+		found, err := EmailToNim(v)
+		if err != nil {
+			t.Errorf("Should not be error on params '%s': %s", v, err)
+		} else {
+			if found != expected {
+				t.Errorf("On input '%s', found '%s', expected '%s'", v, found, expected)
+			}
+		}
+	}
+}
+
+func TestEmailToNimError(t *testing.T) {
+	params := []string{
+		"",
+		"A",
+		"Ab",
+		"Ab.com",
+	}
+	for _, v := range params {
+		_, err := EmailToNim(v)
+		if err == nil {
+			t.Errorf("Should be returning error on params '%s'", v)
+		}
+	}
+}
+
+func TestJenisEmail(t *testing.T) {
+	params := []string{
+		"123456789012@trunojoyo.ac.id",
+		"123456789012@student.trunojoyo.ac.id",
+		"saya@trunojoyo.ac.id",
+		"saya@student.trunojoyo.ac.id",
+		"mereka@gmail.com",
+		"mereka@",
+	}
+	expecteds := []string{
+		"bukan student",
+		"student",
+		"bukan student",
+		"student",
+		"bukan student",
+		"bukan student",
+	}
+	for i := 0; i < len(params); i++ {
+		v := params[i]
+		expected := expecteds[i]
+
+		found, err := JenisEmail(v)
+		if err != nil {
+			t.Errorf("Should not be error on params '%s': %s", v, err)
+		} else {
+			if found != expected {
+				t.Errorf("On input '%s', found '%s', expected '%s'", v, found, expected)
+			}
+		}
+	}
+}
+
+func TestJenisEmailError(t *testing.T) {
+	params := []string{
+		"",
+		"A",
+		"Ab",
+		"Ab.com",
+	}
+	for _, v := range params {
+		_, err := JenisEmail(v)
+		if err == nil {
+			t.Errorf("Should be returning error on params '%s'", v)
+		}
 	}
 }
